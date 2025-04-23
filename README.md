bc9f2f7ea0b4139ad5ddd9342f7262591fafffb9
JYK <jykim9226@gmail.com>
2025-04-23 17:14:40 +0900

    4일차 커밋

--- README.md
+++ README.md
@@ -412,17 +412,100 @@ IoT 개발자 C#/WinApp 리포지토리 2025
     
 ## 4일차
 
+### 윈앱 컨트롤 3
+- `NumericUpDown` : 숫자 입력을 도와주는 컨트롤
+    - Nud~ 이름으로 시작
+    - 나머지는 Button과 동일
+    - Minimum : 입력할 수 있는 최소값
+    - Maxium : 최대값
+    - Value : 현재값
+    - 이벤트 생성 거의 안함
+
+- `RichTextBox` : 포맷팅이 가능한 텍스트박스
+    - MS Word, 한글워드프로세서 같은 문장 꾸미기,
+    페이지 꾸미기가 가능
+    - Rtb~ 로 시작
+    - ScrollBars : 컨트롤 오른쪽, 하단에 스크롤바 생성
+        - Both 보다는 ForcedBoth, ForcedVer를 주로 사용
+    - WordWrap : 컨트롤 길이를 넘어가면 자동으로 줄바꿈 여부
+
+- `OpenFileDialog` : 읽을 파일 위치와 파일을 선정하는 다이얼로그 창 컨트롤
+    -  폼위에 표시되지 않는 컨트롤
+    - DlgOpen 이름으로 사용
+
+- `SaveFileDialog` : 저장 위치에 파일명을 지정하는 다이얼로그 창 컨트롤 
+    - DlgSave 이름으로 사용
+    - 코딩으로 설정내용 작성
+
+- `ColorDialog` : 색상 선택하는 다이얼로그 컨트롤
+    - DlgColor 이름사용
+    - 코딩으로 설정내용 작성
+
+- 대부분 컨트롤 속성
+    - Anchor : 현재 컨트롤을 윈앱 폼의 사이즈가 조정될 때 어디에 고정시키는지 설정
+        - 왼쪽 상단에 있는 컨트롤 : 보통 Left, Top
+        - 오른쪽 하단에 있는 컨트롤 : 보통 Right, Bottom
+    - Dock : 어디에 도킹시키지 설정
+        - Top, Left, Right, Bottom, Fill
+        - 보통 컨테이너 컨트롤용으로 사용
+    - Anchor와 Docker를 같이 사용해서 디자인
+    - Font : 대표적인 폰트 사용할 것
+        - 폼에서 변경하면 하위 컨트롤은 같이 변경
+
+### VS 개발팁
+- 코드조각(Code Snippet)을 최대한 활용(코딩양 감소, 오타 예방)
+- 보기 > 개체 브라우저에서 필요하 클래스 검색
+- 소스코드가 회색으로 연하게 변하는 것 > 되도록이면 사용을 피하라는 뜻 > Alt+Enter로 변경권장사항 확인
+
 ### C# 문법
 - 기본문법
+    0. 형변환 - [소스](./day04/Day04Study/SyntaxWinApp01/FrmMain.cs)
+        - 큰 바이트 데이터형에 작은 바이트 데이터형 값을 할당하면 문제없이 사용가능(묵시적 형변환)
+        - `작은 바이트 데이터형에 큰 바이트 데이터형을 할당하면 문법적 오류` -> (명시적 형변환)
+        - 명시적 형변환 : 실행 중 예외발생은 개발자의 책임!
+        - 값 타입별 .parse(string) : 문자열을 해당 타입으로 변환
+        - Convert 클래스 사용 추천. 여러 타입을 다 변환 가능
+            - Convert.ToInt32()
+        - .TryParse() : 
+    
     1. 예외처리
-    2. 컬렉션
-    3. 파일 입출력
-    4. 델리게이트, 이벤트
-    5. 람다식
-    6. LINQ
-    7. 비동기
-    8. 속성
-    9. 제네릭
+        - 실행중 비상종료를 막기 위해서
+         - try ~ catch ~ finally 구문으로 처리
+         - 모든 예외의 부모클래스는 Exception 클래스
+         - 예외클래스를 모르겠다 싶으면 Exception으로 처리
+
+         <img src="./image/cs0007.png" width="600">
+    
+    2. 컬렉션 - [소스](./day04/Day04Study/SyntaxWinApp02/FrmMain.cs)
+        - 배열, 리스트, 딕셔너리, 큐, 스택 등 여러 데이터를 저장하고 다루는 구조를 가진 객체를 통칭
+
+        - 배열로 콤보박스에 데이터 할당
+        - 리스트로 콤보박스 데이터 할당
+        - 딕셔너리로 콤보박스 데이터 할당
+
+        <img src="./image/cs0009.png" width="600">
+
+    3. 파일 입출력 - [소스](./day04/Day04Study/SyntaxWinApp03/FrmMain.cs)
+        - 파일 쓰기, 읽기 기능
+        - saveFileDilog, OpenFileDilog, ColorDialog 컨트롤 사용
+
+        <img src="./image/cs0010.png" widht="600">
+        
+## 5일차
+
+### C# 문법
+- 기본문법
+    1. 델리게이트, 이벤트
+    2. 람다식
+    3. LINQ
+    4. 비동기
+    5. 속성
+    6. 제네릭
+
+## 6일차
+
+### C# 응용 - WPF
+- 
 
 ## 10일차
 
---
+++ day04/Day04Study/SyntaxWinApp03/FrmMain.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace SyntaxWinApp03
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            BtnSave = new Button();
+            BtnLoad = new Button();
+            RtbNote = new RichTextBox();
+            BtnBold = new Button();
+            BtnRed = new Button();
+            DlgOpen = new OpenFileDialog();
+            DlgSave = new SaveFileDialog();
+            DlgColor = new ColorDialog();
+            SuspendLayout();
+            // 
+            // BtnSave
+            // 
+            BtnSave.Location = new Point(366, 269);
+            BtnSave.Name = "BtnSave";
+            BtnSave.Size = new Size(100, 30);
+            BtnSave.TabIndex = 0;
+            BtnSave.Text = "저장";
+            BtnSave.UseVisualStyleBackColor = true;
+            BtnSave.Click += BtnSave_Click;
+            // 
+            // BtnLoad
+            // 
+            BtnLoad.Location = new Point(472, 269);
+            BtnLoad.Name = "BtnLoad";
+            BtnLoad.Size = new Size(100, 30);
+            BtnLoad.TabIndex = 0;
+            BtnLoad.Text = "읽기";
+            BtnLoad.UseVisualStyleBackColor = true;
+            BtnLoad.Click += BtnLoad_Click;
+            // 
+            // RtbNote
+            // 
+            RtbNote.Font = new Font("맑은 고딕", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            RtbNote.Location = new Point(12, 12);
+            RtbNote.Name = "RtbNote";
+            RtbNote.ScrollBars = RichTextBoxScrollBars.ForcedVertical;
+            RtbNote.Size = new Size(560, 251);
+            RtbNote.TabIndex = 1;
+            RtbNote.Text = "";
+            RtbNote.WordWrap = false;
+            // 
+            // BtnBold
+            // 
+            BtnBold.BackColor = SystemColors.ControlLight;
+            BtnBold.Font = new Font("맑은 고딕", 9F, FontStyle.Bold);
+            BtnBold.Location = new Point(330, 269);
+            BtnBold.Name = "BtnBold";
+            BtnBold.Size = new Size(30, 30);
+            BtnBold.TabIndex = 2;
+            BtnBold.Text = "B";
+            BtnBold.UseVisualStyleBackColor = false;
+            BtnBold.Click += BtnBold_Click;
+            // 
+            // BtnRed
+            // 
+            BtnRed.BackColor = SystemColors.ControlLight;
+            BtnRed.FlatAppearance.BorderColor = Color.Black;
+            BtnRed.Font = new Font("맑은 고딕", 9F, FontStyle.Bold);
+            BtnRed.ForeColor = Color.Red;
+            BtnRed.Location = new Point(294, 269);
+            BtnRed.Name = "BtnRed";
+            BtnRed.Size = new Size(30, 30);
+            BtnRed.TabIndex = 2;
+            BtnRed.Text = "R";
+            BtnRed.UseVisualStyleBackColor = false;
+            BtnRed.Click += BtnRed_Click;
+            // 
+            // DlgOpen
+            // 
+            DlgOpen.FileName = "openFileDialog1";
+            // 
+            // FrmMain
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(584, 311);
+            Controls.Add(BtnRed);
+            Controls.Add(BtnBold);
+            Controls.Add(RtbNote);
+            Controls.Add(BtnLoad);
+            Controls.Add(BtnSave);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Name = "FrmMain";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "파일IO 윈앱";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button BtnSave;
+        private Button BtnLoad;
+        private RichTextBox RtbNote;
+        private Button BtnBold;
+        private Button BtnRed;
+        private OpenFileDialog DlgOpen;
+        private SaveFileDialog DlgSave;
+        private ColorDialog DlgColor;
+    }
+}
