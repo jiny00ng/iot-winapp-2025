242c166bcf598ff124f00cac15904a7efbae377a
JYK <jykim9226@gmail.com>
2025-04-24 17:16:14 +0900

    5일차 커밋

---
+++ day05/Day05Study/SyntaxWinApp02/FrmMain.cs
@@ -0,0 +1,115 @@
+﻿namespace SyntaxWinApp02
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        int add(int x, int y)
+        {
+            return x + y;   // 한줄짜리
+        }
+        private void BtnCheck_Click(object sender, EventArgs e)
+        {
+            int result = add(10, 5);
+
+            // 앞에는 대리자, 뒤에는 람다식
+            Func<int, int, int> add2 = (x, y) => x + y;
+            TxtLog.Text += result + "\r\n";
+            TxtLog.Text += add2(10, 6) + "\r\n";
+
+            // SayHello 함수 생성대신 
+            Action<string> sayHello = name => MessageBox.Show($"안녕, {name}", "인사",
+                                              MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+            sayHello("유고");
+
+            // LINQ 사용 이전
+            List<int> reList = new List<int>();
+            List<int> numbers = [4, 10, 1, 5, 9, 8, 6, 2, 3, 7];
+
+            // 짝수만 추출해서 오름차순 정열을 하는 로직
+            foreach (int n in numbers)
+            {
+                if (n % 2 == 0) // 2로 나눠서 나머지 0이면 짝수
+                {
+                    reList.Add(n);
+                }
+            }
+
+            TxtLog.Text += "전통 짝수 리스트 > " + string.Join(" ", reList) + "\r\n";
+            reList.Sort();
+            TxtLog.Text += "전통 정렬 리스트 > " + string.Join(" ", reList) + "\r\n";
+
+            // 기본 LINQ 방식 > 3줄로 위의 전통방식을 처리
+            numbers = [14, 20, 11, 15, 18, 19, 16, 13, 12, 17];
+            var reList2 = from n in numbers
+                          where n % 2 == 0
+                          orderby n
+                          select n;
+            TxtLog.Text += "링큐 정렬 리스트 > " + string.Join(" ", reList2) + "\r\n";
+
+            // LINQ Method Chaining
+            numbers = [24, 30, 21, 25, 28, 29, 26, 23, 22, 27];
+            var reList3 = numbers.Where(n => n % 2 == 0).OrderBy(n => n);
+            TxtLog.Text += "링큐 정렬 리스트 > " + string.Join(" ", reList3) + "\r\n";
+
+
+        }
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            // UI 초기화
+            TxtTest.PlaceholderText = "테스트입니다";
+            TxtTest.Size = new Size(200, 23);
+            TxtTest.KeyPress += TxtTest_KeyPress; // Designer에서 작업하는 것과 동일
+            TxtTest.Font = new("휴먼매직체", 12, FontStyle.Italic);
+        }
+        private void TxtTest_KeyPress(object? sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter) // == 13과 동일
+            {
+                MessageBox.Show("엔터를 클릭했습니다.", "키프레스",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+        private void BtnGeneric_Click(object? sender, EventArgs e)
+        {
+            // 기본
+            Print<int>(100);
+            Print<float>(3.141592f);
+            Print<string>("안녕, 유고!");
+            Print<bool>(false);
+
+            // 생략가능 - 편의성을 위해서
+            Print<int>(100);
+            Print(3.141592f);
+            Print("안녕, 유고!");
+            Print(false);
+
+            // 제네릭 클래스 사용
+            Box<int> intBox = new Box<int>();
+            intBox.Value = 300;
+            intBox.Show();
+
+            Box<string> strBox = new Box<string>();
+            strBox.Value = "애슐리";
+            strBox.Show();
+        }
+
+        public void Print<T>(T data) { Console.WriteLine(data); }
+    }
+
+    // 제네릭 클래스
+    public class Box<T> // where T : struct
+    { 
+        public T Value { get; set; }    // 속성
+        // public T value;  // 멤버변수
+        public void Show()
+        {
+            Console.WriteLine($"Box의 값 : {Value}", "Box값",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+
+}
---
+++ day05/Day05Study/SyntaxWinApp03/FrmMain.cs
@@ -0,0 +1,53 @@
+﻿namespace SyntaxWinApp03
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        // async 비동기로 동작하는 메서드라고 서언
+        // async, await는 항상 같이 사용
+        private async void BtnStart_Click(object sender, EventArgs e)
+        {
+            // UI 스레드가 관리
+            LblCurrState.Text = "현재상태 : 진행";    // UI 처리
+            BtnStart.Text = "진행중";  // UI 처리
+            BtnStart.Enabled = false;   // 못쓰게 함    // UI 처리
+
+            // 엄청난 시간이 걸리는 연산을 수행
+            long MaxVal = 200;
+            long total = 0;
+            PrgProcess.Minimum = 0;     // UI 처리
+            PrgProcess.Maximum = 100;   // UI 처리
+
+            // await로 비동기 대기
+            await Task.Run(() =>
+            {
+                for (int i = 0; i < MaxVal; i++)
+                {
+                    total += i % 3;
+
+                    int progress = (int)((i * 100) / MaxVal) + 1;
+                    Console.WriteLine(progress.ToString());
+                    // Task.Run 내에 들어가는 UI처리 로직은
+                    this.Invoke(new Action(() =>
+                    {
+                        TxtLog.Text += i.ToString() + "\r\n";
+                        TxtLog.SelectionStart = TxtLog.Text.Length;
+                        TxtLog.ScrollToCaret(); // 스크롤을 제일 밑으로
+                        PrgProcess.Value = progress;
+                    }));
+
+                    Thread.Sleep(50);  // 50ms 동안 대기(대량의 일을 처리하는 것처럼)
+                    // Application.DoEvents();  // 사용하지 말것
+                }
+            });
+
+            LblCurrState.Text = "현재상태 : 중지";    // UI 처리
+            BtnStart.Text = "시작";   // UI 처리
+            BtnStart.Enabled = true;    // UI 처리
+        }
+    }
+}
