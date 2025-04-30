3d5c9c68d270d2df2ba8b28e64d37ccc9846e9bd
JYK <jykim9226@gmail.com>
2025-04-30 17:33:33 +0900

    MyWinApp

---
+++ toyproject/MyWinApp/MyWinFormsApp/FrmMain.Designer.cs
@@ -0,0 +1,357 @@
+﻿namespace MyWinFormsApp
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            groupBox1 = new GroupBox();
+            label1 = new Label();
+            pictureBox2 = new PictureBox();
+            BtnSaveSchedule = new Button();
+            BtnDeleteSchedule = new Button();
+            TxtInput = new TextBox();
+            DtpSchedule = new DateTimePicker();
+            CalSchedule = new MonthCalendar();
+            groupBox2 = new GroupBox();
+            label2 = new Label();
+            TxtDay = new TextBox();
+            TxtAllSchedules = new TextBox();
+            TxtDaySchedules = new TextBox();
+            BtnViewAllSchedules = new Button();
+            BtnOpenImage = new Button();
+            PicImage = new PictureBox();
+            button1 = new Button();
+            statusStrip1 = new StatusStrip();
+            toolStripStatusLabel1 = new ToolStripStatusLabel();
+            menuStrip1 = new MenuStrip();
+            MnuFile = new ToolStripMenuItem();
+            MnuNewFile = new ToolStripMenuItem();
+            ctrlNToolStripMenuItem = new ToolStripMenuItem();
+            toolStripMenuItem2 = new ToolStripSeparator();
+            MnuExit = new ToolStripMenuItem();
+            ctrlXToolStripMenuItem = new ToolStripMenuItem();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)PicImage).BeginInit();
+            statusStrip1.SuspendLayout();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(pictureBox2);
+            groupBox1.Controls.Add(BtnSaveSchedule);
+            groupBox1.Controls.Add(BtnDeleteSchedule);
+            groupBox1.Controls.Add(TxtInput);
+            groupBox1.Controls.Add(DtpSchedule);
+            groupBox1.Controls.Add(CalSchedule);
+            groupBox1.Location = new Point(12, 34);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(450, 502);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "입력";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 246);
+            label1.Name = "label1";
+            label1.Size = new Size(59, 15);
+            label1.TabIndex = 5;
+            label1.Text = "일정 입력";
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = SystemColors.Window;
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(238, 28);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(206, 197);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 4;
+            pictureBox2.TabStop = false;
+            // 
+            // BtnSaveSchedule
+            // 
+            BtnSaveSchedule.Location = new Point(344, 472);
+            BtnSaveSchedule.Name = "BtnSaveSchedule";
+            BtnSaveSchedule.Size = new Size(100, 30);
+            BtnSaveSchedule.TabIndex = 3;
+            BtnSaveSchedule.Text = "저장";
+            BtnSaveSchedule.UseVisualStyleBackColor = true;
+            BtnSaveSchedule.Click += BtnSaveSchedule_Click;
+            // 
+            // BtnDeleteSchedule
+            // 
+            BtnDeleteSchedule.Location = new Point(12, 472);
+            BtnDeleteSchedule.Name = "BtnDeleteSchedule";
+            BtnDeleteSchedule.Size = new Size(100, 30);
+            BtnDeleteSchedule.TabIndex = 3;
+            BtnDeleteSchedule.Text = "삭제";
+            BtnDeleteSchedule.UseVisualStyleBackColor = true;
+            BtnDeleteSchedule.Click += BtnDeleteSchedule_Click;
+            // 
+            // TxtInput
+            // 
+            TxtInput.Location = new Point(12, 264);
+            TxtInput.Multiline = true;
+            TxtInput.Name = "TxtInput";
+            TxtInput.ScrollBars = ScrollBars.Vertical;
+            TxtInput.Size = new Size(432, 202);
+            TxtInput.TabIndex = 2;
+            // 
+            // DtpSchedule
+            // 
+            DtpSchedule.Location = new Point(12, 202);
+            DtpSchedule.Name = "DtpSchedule";
+            DtpSchedule.Size = new Size(220, 23);
+            DtpSchedule.TabIndex = 1;
+            // 
+            // CalSchedule
+            // 
+            CalSchedule.Location = new Point(12, 28);
+            CalSchedule.Name = "CalSchedule";
+            CalSchedule.TabIndex = 0;
+            CalSchedule.DateChanged += CalSchedule_DateChanged;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(label2);
+            groupBox2.Controls.Add(TxtDay);
+            groupBox2.Controls.Add(TxtAllSchedules);
+            groupBox2.Controls.Add(TxtDaySchedules);
+            groupBox2.Controls.Add(BtnViewAllSchedules);
+            groupBox2.Controls.Add(BtnOpenImage);
+            groupBox2.Controls.Add(PicImage);
+            groupBox2.Controls.Add(button1);
+            groupBox2.Location = new Point(468, 34);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(704, 502);
+            groupBox2.TabIndex = 0;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "일정";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(289, 25);
+            label2.Name = "label2";
+            label2.Size = new Size(59, 15);
+            label2.TabIndex = 7;
+            label2.Text = "오늘 일정";
+            // 
+            // TxtDay
+            // 
+            TxtDay.Location = new Point(354, 22);
+            TxtDay.Name = "TxtDay";
+            TxtDay.Size = new Size(139, 23);
+            TxtDay.TabIndex = 6;
+            // 
+            // TxtAllSchedules
+            // 
+            TxtAllSchedules.Location = new Point(6, 295);
+            TxtAllSchedules.Multiline = true;
+            TxtAllSchedules.Name = "TxtAllSchedules";
+            TxtAllSchedules.ScrollBars = ScrollBars.Vertical;
+            TxtAllSchedules.Size = new Size(692, 201);
+            TxtAllSchedules.TabIndex = 5;
+            // 
+            // TxtDaySchedules
+            // 
+            TxtDaySchedules.Location = new Point(289, 51);
+            TxtDaySchedules.Multiline = true;
+            TxtDaySchedules.Name = "TxtDaySchedules";
+            TxtDaySchedules.ScrollBars = ScrollBars.Vertical;
+            TxtDaySchedules.Size = new Size(409, 207);
+            TxtDaySchedules.TabIndex = 5;
+            // 
+            // BtnViewAllSchedules
+            // 
+            BtnViewAllSchedules.Location = new Point(289, 264);
+            BtnViewAllSchedules.Name = "BtnViewAllSchedules";
+            BtnViewAllSchedules.Size = new Size(99, 25);
+            BtnViewAllSchedules.TabIndex = 3;
+            BtnViewAllSchedules.Text = "전체 일정 보기";
+            BtnViewAllSchedules.UseVisualStyleBackColor = true;
+            BtnViewAllSchedules.Click += BtnViewAllSchedules_Click;
+            // 
+            // BtnOpenImage
+            // 
+            BtnOpenImage.Location = new Point(6, 264);
+            BtnOpenImage.Name = "BtnOpenImage";
+            BtnOpenImage.Size = new Size(90, 25);
+            BtnOpenImage.TabIndex = 3;
+            BtnOpenImage.Text = "이미지 열기";
+            BtnOpenImage.UseVisualStyleBackColor = true;
+            BtnOpenImage.Click += BtnOpenImage_Click;
+            // 
+            // PicImage
+            // 
+            PicImage.BackColor = SystemColors.ActiveBorder;
+            PicImage.Location = new Point(6, 22);
+            PicImage.Name = "PicImage";
+            PicImage.Size = new Size(277, 236);
+            PicImage.TabIndex = 4;
+            PicImage.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(116, 452);
+            button1.Name = "button1";
+            button1.Size = new Size(8, 8);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.Items.AddRange(new ToolStripItem[] { toolStripStatusLabel1 });
+            statusStrip1.Location = new Point(0, 539);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new Size(1184, 22);
+            statusStrip1.SizingGrip = false;
+            statusStrip1.TabIndex = 1;
+            statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            toolStripStatusLabel1.Size = new Size(90, 17);
+            toolStripStatusLabel1.Text = "상태 : 일반상태";
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { MnuFile });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(1184, 24);
+            menuStrip1.TabIndex = 2;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // MnuFile
+            // 
+            MnuFile.DropDownItems.AddRange(new ToolStripItem[] { MnuNewFile, toolStripMenuItem2, MnuExit });
+            MnuFile.Name = "MnuFile";
+            MnuFile.Size = new Size(57, 20);
+            MnuFile.Text = "파일(&F)";
+            // 
+            // MnuNewFile
+            // 
+            MnuNewFile.DropDownItems.AddRange(new ToolStripItem[] { ctrlNToolStripMenuItem });
+            MnuNewFile.Name = "MnuNewFile";
+            MnuNewFile.Size = new Size(119, 22);
+            MnuNewFile.Text = "새 글(&N)";
+            // 
+            // ctrlNToolStripMenuItem
+            // 
+            ctrlNToolStripMenuItem.Name = "ctrlNToolStripMenuItem";
+            ctrlNToolStripMenuItem.Size = new Size(118, 22);
+            ctrlNToolStripMenuItem.Text = "Ctrl + N";
+            // 
+            // toolStripMenuItem2
+            // 
+            toolStripMenuItem2.Name = "toolStripMenuItem2";
+            toolStripMenuItem2.Size = new Size(116, 6);
+            // 
+            // MnuExit
+            // 
+            MnuExit.DropDownItems.AddRange(new ToolStripItem[] { ctrlXToolStripMenuItem });
+            MnuExit.Name = "MnuExit";
+            MnuExit.Size = new Size(119, 22);
+            MnuExit.Text = "종료(&X)";
+            MnuExit.Click += MnuExit_Click;
+            // 
+            // ctrlXToolStripMenuItem
+            // 
+            ctrlXToolStripMenuItem.Name = "ctrlXToolStripMenuItem";
+            ctrlXToolStripMenuItem.Size = new Size(116, 22);
+            ctrlXToolStripMenuItem.Text = "Ctrl + X";
+            // 
+            // FrmMain
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.GradientInactiveCaption;
+            ClientSize = new Size(1184, 561);
+            Controls.Add(statusStrip1);
+            Controls.Add(menuStrip1);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MainMenuStrip = menuStrip1;
+            MaximizeBox = false;
+            Name = "FrmMain";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "일정 관리";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)PicImage).EndInit();
+            statusStrip1.ResumeLayout(false);
+            statusStrip1.PerformLayout();
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private DateTimePicker DtpSchedule;
+        private MonthCalendar CalSchedule;
+        private Button BtnSaveSchedule;
+        private Button BtnDeleteSchedule;
+        private TextBox TxtInput;
+        private Button button1;
+        private PictureBox PicImage;
+        private PictureBox pictureBox2;
+        private TextBox TxtDaySchedules;
+        private Button BtnOpenImage;
+        private Label label2;
+        private TextBox TxtDay;
+        private Button BtnViewAllSchedules;
+        private Label label1;
+        private StatusStrip statusStrip1;
+        private ToolStripStatusLabel toolStripStatusLabel1;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem MnuFile;
+        private ToolStripMenuItem MnuNewFile;
+        private ToolStripMenuItem ctrlNToolStripMenuItem;
+        private ToolStripSeparator toolStripMenuItem2;
+        private ToolStripMenuItem MnuExit;
+        private ToolStripMenuItem ctrlXToolStripMenuItem;
+        private TextBox TxtAllSchedules;
+    }
+}
---
+++ toyproject/MyWinApp/MyWinFormsApp/FrmMain.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace MyWinFormsApp
+{
+    public partial class FrmMain : Form
+    {
+        private OpenFileDialog DlgOpenImage;
+        private Dictionary<DateTime, string> schedules;  // 일정 저장을 위한 Dictionary
+        private Dictionary<DateTime, Image> images;  // 날짜별로 이미지 저장을 위한 Dictionary
+
+        public FrmMain()
+        {
+            InitializeComponent();
+            DlgOpenImage = new OpenFileDialog();
+            schedules = new Dictionary<DateTime, string>();  // 일정 Dictionary 초기화
+            images = new Dictionary<DateTime, Image>();  // 이미지 Dictionary 초기화
+
+            // 이벤트 핸들러 연결
+            DtpSchedule.ValueChanged += DtpSchedule_ValueChanged;  // DateTimePicker 값 변경 시 호출될 이벤트 핸들러
+            MnuExit.Click += MnuExit_Click;  // Exit 메뉴 클릭 시 호출될 이벤트 핸들러
+        }
+
+        // 이미지 열기
+        private void BtnOpenImage_Click(object sender, EventArgs e)
+        {
+            DlgOpenImage.Title = "이미지 열기";
+            DlgOpenImage.FileName = "";
+            DlgOpenImage.Filter = "Image Files(*.bmp;*.jpg;*.png)|*.bmp;*.jpg;*.png";
+
+            if (DlgOpenImage.ShowDialog() == DialogResult.OK)
+            {
+                PicImage.Image = Bitmap.FromFile(DlgOpenImage.FileName);  // 선택한 파일을 PictureBox에 표시
+                PicImage.SizeMode = PictureBoxSizeMode.Zoom;  // 이미지 크기 맞춰서 표시
+                DateTime selectedDate = CalSchedule.SelectionStart;  // 선택된 날짜 가져오기
+                images[selectedDate] = PicImage.Image;  // 해당 날짜에 이미지 저장
+            }
+        }
+
+        // 날짜 변경 시 일정 표시
+        private void CalSchedule_DateChanged(object sender, EventArgs e)
+        {
+            DateTime selectedDate = CalSchedule.SelectionStart;  // 선택된 날짜 가져오기
+            TxtDay.Text = selectedDate.ToString("yyyy-MM-dd");
+
+            // 선택된 날짜의 일정 표시
+            if (schedules.ContainsKey(selectedDate))
+            {
+                TxtInput.Text = schedules[selectedDate];  // 일정 표시
+            }
+            else
+            {
+                TxtInput.Text = "";  // 일정이 없으면 텍스트 박스 비우기
+            }
+
+            // 선택된 날짜의 이미지 표시
+            if (images.ContainsKey(selectedDate))
+            {
+                PicImage.Image = images[selectedDate];  // 해당 날짜에 저장된 이미지 표시
+            }
+            else
+            {
+                PicImage.Image = null;  // 이미지가 없으면 비워두기
+            }
+        }
+
+        // 일정 저장
+        private void BtnSaveSchedule_Click(object sender, EventArgs e)
+        {
+            DateTime selectedDate = DtpSchedule.Value.Date;  // 선택된 날짜 가져오기
+            string schedule = TxtInput.Text.Trim();  // 일정 입력값
+
+            if (!string.IsNullOrEmpty(schedule))
+            {
+                // 일정 저장
+                if (schedules.ContainsKey(selectedDate))
+                {
+                    schedules[selectedDate] = schedule;  // 기존 일정 덮어쓰기
+                }
+                else
+                {
+                    schedules.Add(selectedDate, schedule);  // 새로운 일정 추가
+                }
+
+                MessageBox.Show("일정이 저장되었습니다!", "알림", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("일정을 입력해주세요.", "알림", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        // 전체 일정 보기
+        private void BtnViewAllSchedules_Click(object sender, EventArgs e)
+        {
+            string allSchedules = string.Join(Environment.NewLine, schedules.Select(kv => $"{kv.Key:yyyy-MM-dd}: {kv.Value}"));
+            TxtAllSchedules.Text = allSchedules;  // 전체 일정 표시
+        }
+
+        // 일정 삭제
+        private void BtnDeleteSchedule_Click(object sender, EventArgs e)
+        {
+            DateTime selectedDate = DtpSchedule.Value.Date;  // 선택된 날짜 가져오기
+
+            if (schedules.ContainsKey(selectedDate))
+            {
+                schedules.Remove(selectedDate);  // 일정 삭제
+                TxtInput.Clear();  // 텍스트박스 비우기
+                PicImage.Image = null;  // 이미지 삭제
+                MessageBox.Show("일정이 삭제되었습니다.", "알림", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("선택된 날짜에 일정이 없습니다.", "알림", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        // 종료 메뉴 클릭 시
+        private void MnuExit_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("종료하시겠습니까?", "종료여부",
+                                MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                this.FormClosing -= FrmMain_FormClosing; // 폼클로징 이벤트를 삭제
+                Application.Exit(); // 프로그램 완전 종료
+            }
+        }
+
+        // 폼 종료 시 확인
+        private void FrmMain_FormClosing(object? sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("종료하시겠습니까?", "종료여부",
+                                MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No)
+            {
+                e.Cancel = true;    // 종료를 안 시키는 것
+            }
+        }
+    }
+}
